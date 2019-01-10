223b063261e61b609b11405e9ced450d0498289e
maissakh <45101409+maissakh@users.noreply.github.com>
2019-01-10 15:21:26 +0100

    Update Catapulte.cpp

--- Catapulte.cpp
+++ Catapulte.cpp
@@ -6,7 +6,10 @@ using namespace std;
 Catapulte::Catapulte() // pb sur les portées
 
 {
-    prix = 2;
-
+    prix = 20;
+    vie =12;
+    attaque = 6;
+    porteeMin = 2;// 2 a 3 ou 3 a 4 ... est ce que ca marche pour nous ?!
+    porteeMax = 3;
  
 }
