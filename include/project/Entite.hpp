a492491d111b6135720d51b100c42e49b5f763b7
maissakh <45101409+maissakh@users.noreply.github.com>
2019-01-27 19:34:29 +0100

    Update Entite.hpp

--- include/project/Entite.hpp
+++ include/project/Entite.hpp
@@ -18,15 +18,14 @@ public:
 	
 	Entite();
 	Entite(std::string nom, int camp);
-    int getCamps();
-
+	int getCamps();
 	int getVie();
-    std::string getNom();
-    void setCamps(int val);
-    void setVie (int tmpvie);
-    void setNom (std::string name);
-    virtual bool perdreVie(int vie) =0;
-    bool estMorte();
+	std::string getNom();
+    	void setCamps(int val);
+   	void setVie (int tmpvie);
+    	void setNom (std::string name);
+    	virtual bool perdreVie(int vie) =0;
+    	bool estMorte();
 
 
 
