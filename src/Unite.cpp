2a47f2300a696c7a986bf5cdf9a5a7b7aa99022f
maissakh <45101409+maissakh@users.noreply.github.com>
2019-01-27 19:31:47 +0100

    Update Unite.cpp

--- src/Unite.cpp
+++ src/Unite.cpp
@@ -1,3 +1,33 @@
-#include "../include/project/Unite.hpp"
+#include "project/Unite.hpp"
 
 Unite::Unite() {}
+
+int Unite::getAttaque (){
+
+	return attaque;
+}
+
+int Unite::getPosition () {
+	return position;
+}
+
+
+int Unite::getPorteeMin() {
+	return porteeMin;
+
+}
+
+int Unite::getPorteeMax() {
+	return porteeMax;
+}
+void Unite::setStatut (){
+	statut=false;
+}
+
+bool Unite:: getStatut(){
+	return statut; 
+}
+
+void Unite:: setPosition (int pos){
+	position=pos;
+}
