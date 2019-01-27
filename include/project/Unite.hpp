dbae6e1412aff0fc5de09a0a0843bb2deaf06fb1
maissakh <45101409+maissakh@users.noreply.github.com>
2019-01-27 19:35:02 +0100

    Update Unite.hpp

--- include/project/Unite.hpp
+++ include/project/Unite.hpp
@@ -8,17 +8,26 @@ class Unite : public Entite
 	
 public:
 	Unite();
+ 	virtual ~Unite();
 	virtual bool avancer() =0;
 	virtual int attaquer () =0;
+	int getAttaque ();
+	int getPosition ();
+	void setPosition (int pos);
+	int getPorteeMin();
+	int getPorteeMax();
+	void setStatut ();
+	bool getStatut();
 
 protected:
-    int prix;
+
+	int prix;
 	int porteeMin;
 	int porteeMax;
 	static int id;
 	int attaque;
 	int position;
-
+	bool statut;
 
 };
 
